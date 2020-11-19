d24afb5ca4dac410234901c3d3e229b06ef6bbc5
012022581905 <ng.huu.anh.khoa@valuence.inc>
2020-11-20 00:49:13 +0700

    add click change item slide

---
+++ assets/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let indexSlide = 0;
+const prevBtn = document.querySelector('.btncontroll .btncontroll__item #prevBtn');
+const nextBtn = document.querySelector('.btncontroll .btncontroll__item #nextBtn');
+const listDot = document.querySelectorAll('.dotcontroll .dotcontroll__wrapper');
+const mySlides = document.querySelectorAll('#slide .slide__item');
+showSlide(indexSlide);
+function showSlide(n) {
+    if (n > mySlides.length - 1) {
+        n = 0;
+        indexSlide = -1;
+    };
+    if (n < 0) {
+        n = mySlides.length - 1;
+        indexSlide = mySlides.length - 1;
+    };
+    mySlides.forEach(item => {
+        item.classList.remove('active-slide');
+    });
+    mySlides[n].classList.add('active-slide');
+};
+prevBtn.addEventListener('click', function () {
+    showSlide(--indexSlide);
+});
+
+nextBtn.addEventListener('click', function () {
+    showSlide(++indexSlide);
+});
